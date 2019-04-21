c9df67fe6e7c7ba5808cd0aa46135282522e17dc
Afroze Khan <a.froze.ak@gmail.com>
2019-04-21 19:27:04 -0400

    swapped css files with 1 sass file

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,17 +11,22 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import 'bootstrap/dist/css/bootstrap.css';
+import "../styles/main.scss";
+
 import 'jquery/dist/jquery.min.js';
-import "./layout.css"
+// import "./layout.css"
 
 
 
 const Layout = ({ children }) => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-4">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light flex-column">
-          <a className="navbar-brand" href="#">Navbar</a>
+  <div className="container-fluid h-100">
+    <div className="row h-100">
+      <div id="nav-container" className="col-md-3 h-100">
+        <nav className="navbar navbar-expand-lg navbar-light flex-column">
+          <a className="navbar-brand" href="/">
+            <h1>AK</h1>
+            <h2>Afroze Khan</h2>
+          </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -29,22 +34,22 @@ const Layout = ({ children }) => (
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav flex-column">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">Projects</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
+                <a className="nav-link" href="#">Skillset</a>
+              </li>  
               <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
+                <a className="nav-link" href="#"><img src="/images/gatsby-icon.png" alt=""/></a>
+              </li>          
             </ul>
           </div>
         </nav>
       </div>
-      <div className="col-md-8">
+      <div className="col-md-9">
         {children}
       </div>
     </div>
