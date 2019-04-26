fc8ad22b938ec625131860597fb67e80f138e003
Afroze Khan <a.froze.ak@gmail.com>
2019-04-26 16:00:22 -0400

    changed meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Afroze Khan`,
+    description: `The Official`,
+    author: `Afroze Khan`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
